aca2f0b3774f875eea9cd867609205db50de78a8
aomou <100065388+aomou@users.noreply.github.com>
2025-11-13 18:05:39 +0800

    Move config data into separate module

--- app.py
+++ app.py
@@ -5,21 +5,11 @@ import pandas as pd
 import re
 import streamlit as st
 
+from config_data import season_dic, member_order, multiple_color
+
 st.header("Get Objekt Color")
 
 url = "https://apollo.cafe/api/objekts/"
-
-season_dic = {'A': 'Atom01', 'B': 'Binary01', 'C': 'Cream01', 'D': 'Divine01', 'E': 'Ever01',
-              'AA': 'Atom02', 'BB': 'Binary02'}
-member_order = [
-    'SeoYeon', 'HyeRin', 'JiWoo', 'ChaeYeon', 'YooYeon', 'SooMin', 'NaKyoung', 'YuBin', 
-    'Kaede', 'DaHyun', 'Kotone', 'YeonJi', 'Nien', 'SoHyun', 'Xinyu', 'Mayu',
-    'Lynn', 'JooBin', 'HaYeon', 'ShiOn', 'ChaeWon', 'Sullin', 'SeoAh', 'JiYeon',
-    'HeeJin', 'HaSeul', 'KimLip', 'JinSoul', 'Choerry',
-    # 'DoHun', 'HeeJu', 'TaeIn', 'JaeYoung', 'JuHo', 'JiWoon', 'HwanHee',
-    'tripleS', 'AAA', '+(KR)E', 'TokyoHaus', 'Assemble24', 'S21', 'S22', 'S23', 'S24',
-]
-multiple_color = ['C324Z', 'D304A', 'D322A', 'D328A', 'D334A']
 
 # define functions
 
